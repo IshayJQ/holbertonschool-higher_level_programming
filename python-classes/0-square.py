db0ca8ec39e15bf8a3327838efb1a5a3d6c1b0c7
IshayJQ <113075680+IshayJQ@users.noreply.github.com>
2023-10-19 09:48:57 -0500

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Creating a class that represents a square
+"""
+
+class Square:
+    """
+    The class that represents a square
+    """
+    pass
 Square = __import__('0-square').Square
 
 my_square = Square()
