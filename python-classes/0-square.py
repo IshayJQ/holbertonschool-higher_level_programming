762390f467b1eea2b75951a106022257e7202b6a
IshayJQ <113075680+IshayJQ@users.noreply.github.com>
2023-10-20 20:47:08 -0500

    Python - Classes and Objects

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,9 +8,3 @@ class Square:
     The class that represents a square
     """
     pass
-Square = __import__('0-square').Square
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
-
