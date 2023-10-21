d5559fcc51754b8a3dacaf88551fc1d84de6b61c
IshayJQ <113075680+IshayJQ@users.noreply.github.com>
2023-10-20 20:58:21 -0500

    Correction

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,8 +5,8 @@
 
 class Square:
     """The class that represents a square"""
-    
-    def __init__(self, size):
+
+    def __init__(self, size=0):
         """Initialize a new Square.
 
         Args:
@@ -17,4 +17,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
- 
