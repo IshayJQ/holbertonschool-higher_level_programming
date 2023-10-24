e3ba015b1f464f618f46f99ea3161713d354c021
IshayJQ <113075680+IshayJQ@users.noreply.github.com>
2023-10-23 20:43:41 -0500

    Correction

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,7 +9,6 @@ class BaseGeometry:
         """Not yet implemented."""
         raise Exception("area() is not implemented")
 
-
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
 
